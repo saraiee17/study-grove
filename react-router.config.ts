1ffa4be41387bc002964cccbc8bb8256b6f83105
Sarai Escobar <saraiee17@yahoo.com>
2025-06-20 12:12:35 -0500

    fix: Configure project for react-router build system

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+  appDirectory: "app",
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
 } satisfies Config;
